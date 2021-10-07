27b296e07f2b33760c699c70b24aaf0d46394430
jpatidar <jpatidar@gmail.com>
2021-10-06 22:44:08 -0400

    testApplicationLearning

---
+++ src/test/java/com/techefx/microservices/propertyaccess/TechefxPropertyAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techefx.microservices.propertyaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechefxPropertyAccessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
